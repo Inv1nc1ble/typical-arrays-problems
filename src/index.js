a7b369e6e79ee5bd7d409b6f27b85460e37f8e5f
Stas <stasprokopyshyn@gmail.com>
2021-06-29 17:10:42 +0300

    task is done v1.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) {return 0;}
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) {return 0;}
+  return Math.max.apply(0, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length == 0) {return 0;}
+  return array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue }, 0) / array.length;
 }
